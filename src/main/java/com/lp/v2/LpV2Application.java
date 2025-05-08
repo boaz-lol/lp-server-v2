791529acddb0b16451a9ab291b7cb344abf7c3b9
sunnyineverywhere <sunnyinsummer@ewhain.net>
2025-05-08 17:07:37 +0900

    Initializes the Spring Boot project
    Sets up the basic structure for a new Spring Boot application.
    Includes initial configurations for Gradle, git, and core application components.

---
+++ src/main/java/com/lp/v2/LpV2Application.java
@@ -0,0 +1,13 @@
+package com.lp.v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpV2Application.class, args);
+	}
+
+}
