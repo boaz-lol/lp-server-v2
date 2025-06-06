88bf94e26fa66f3ff83bf149861648370db7bc73
sunnyineverywhere <sunnyinsummer@ewhain.net>
2025-06-07 00:01:29 +0900

    fix:  updates account creation endpoint response with base

--- src/main/java/com/lp/v2/domains/account/AccountController.java
+++ src/main/java/com/lp/v2/domains/account/AccountController.java
@@ -1,5 +1,7 @@
 package com.lp.v2.domains.account;
 
+import com.lp.v2.common.response.BaseResponse;
+import com.lp.v2.common.response.ResponseMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +17,10 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping
-    public ResponseEntity<String> createAccount(@RequestBody AccountCreateReq req) {
+    public ResponseEntity<BaseResponse> createAccount(@RequestBody AccountCreateReq req) {
         accountService.create(req);
-        return ResponseEntity.ok("계정 생성이 완료되었습니다.");
+        return ResponseEntity.ok(
+                BaseResponse.success(ResponseMessage.ACCOUNT_CREATED_SUCCESS)
+        );
     }
 }
