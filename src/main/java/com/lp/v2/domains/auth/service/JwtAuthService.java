f83dff7075536ad42c3aa376867bc4a496d6d4c3
sunnyineverywhere <sunnyinsummer@ewhain.net>
2025-06-08 01:45:33 +0900

    refactor: renames JwtService to JwtAuthService
    Updates the name of JwtService to JwtAuthService
    for better clarity and consistency with naming conventions.
    Also, it switches from using JwtProvider to JwtManager
    for handling JWT-related operations.

--- src/main/java/com/lp/v2/domains/auth/service/JwtAuthService.java
+++ src/main/java/com/lp/v2/domains/auth/service/JwtAuthService.java
@@ -1,20 +1,20 @@
 package com.lp.v2.domains.auth.service;
 
 import com.lp.v2.domains.auth.dto.TokenPair;
-import com.lp.v2.security.jwt.JwtProvider;
+import com.lp.v2.security.jwt.JwtManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-class JwtService {
-    private final JwtProvider jwtProvider;
+class JwtAuthService {
+    private final JwtManager jwtManager;
     private static final String BEARER_PREFIX = "Bearer ";
 
     TokenPair create(Long accountId) {
         return new TokenPair (
-                BEARER_PREFIX + jwtProvider.createAccessToken(accountId.toString()),
-                jwtProvider.createRefreshToken(accountId.toString())
+                BEARER_PREFIX + jwtManager.createAccessToken(accountId.toString()),
+                jwtManager.createRefreshToken(accountId.toString())
         );
     }
 
