3914958abe5d56b2f438d600fbb8e257a72af2db
sunnyineverywhere <sunnyinsummer@ewhain.net>
2025-06-10 00:53:30 +0900

    refactor: simplifies account controller responses.

--- src/main/java/com/lp/v2/domains/account/AccountController.java
+++ src/main/java/com/lp/v2/domains/account/AccountController.java
@@ -4,6 +4,7 @@ import com.lp.v2.common.response.BaseResponse;
 import com.lp.v2.common.response.ResponseMessage;
 import com.lp.v2.security.annotation.CurrentAccountId;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,46 +16,39 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping("/signup")
-    public ResponseEntity<BaseResponse> createAccount(@RequestBody AccountCreateReq req) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createAccount(@RequestBody AccountCreateReq req) {
         accountService.create(req);
-        return ResponseEntity.ok(
-                BaseResponse.success(ResponseMessage.ACCOUNT_CREATED_SUCCESS)
-        );
     }
 
     @GetMapping("/me")
-    public ResponseEntity<AccountInfoRes> getMyInfo(@CurrentAccountId Long accountId) {
-        return ResponseEntity.ok().body(accountService.getInfo(accountId));
+    @ResponseStatus(HttpStatus.OK)
+    public AccountInfoRes getMyInfo(@CurrentAccountId Long accountId) {
+        return accountService.getInfo(accountId);
     }
 
     @PostMapping("/verify")
-    public ResponseEntity<BaseResponse> verifyPassword(
+    @ResponseStatus(HttpStatus.OK)
+    public void verifyPassword(
             @CurrentAccountId Long accountId,
             @RequestBody PasswordVerifyReq req
     ) {
         accountService.verifyPassword(accountId, req.password());
-        return ResponseEntity.ok(
-                BaseResponse.success(ResponseMessage.PASSWORD_VERIFICATION_SUCCESS)
-        );
     }
 
     @PutMapping("/me")
-    public ResponseEntity<BaseResponse> updateMyInfo(
+    @ResponseStatus(HttpStatus.OK)
+    public void updateMyInfo(
             @CurrentAccountId Long accountId,
             @RequestBody AccountUpdateReq req
     ) {
         accountService.update(accountId, req);
-        return ResponseEntity.ok(
-                BaseResponse.success(ResponseMessage.ACCOUNT_UPDATE_SUCCESS)
-        );
     }
 
     @DeleteMapping("/me")
-    public ResponseEntity<BaseResponse> deleteMyAccount(@CurrentAccountId Long accountId) {
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteMyAccount(@CurrentAccountId Long accountId) {
         accountService.delete(accountId);
-        return ResponseEntity.ok(
-                BaseResponse.success(ResponseMessage.ACCOUNT_DELETE_SUCCESS)
-        );
     }
 
 }
