df8bf5728e727a3250193fad4c1f25a5e00438a6
sunnyineverywhere <sunnyinsummer@ewhain.net>
2026-02-18 16:04:23 +0900

    refactor: 신규 프로젝트 생성

---
+++ src/main/kotlin/com/boaz/lp/LoLMatchDataSearchServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.boaz.lp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoLMatchDataSearchServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoLMatchDataSearchServiceApplication>(*args)
+}
